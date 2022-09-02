2a3945c3ec565fae7af33a49b719bf90f230293d
Dafarlog <Dafarlog@gmail.com>
2022-09-02 20:21:04 +0300

    add setFeedBackFromLocaleStorege

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,40 +8,41 @@ const feedbackObj = {
         email: "",
         message: "",        
 }
+setFeedbackObjFromLocaleStorege()
+const {email, message} = feedbackObj
 
-feedbackEmailInput.value =feedbackObj.email;
-feedbackMessageInput.value =feedbackObj.message;
+feedbackEmailInput.value =email;
+feedbackMessageInput.value =message;
 
 feedbackForm.addEventListener("submit", onSubmit)
-
-feedbackForm.addEventListener("input",  throttle((event) => onInput(event), 1000))
+feedbackForm.addEventListener("input",  throttle((event)=>onInput(event), 1000))
 
 function onSubmit(event){
     event.preventDefault();
     console.log(JSON.parse(localStorage.getItem(KEY_FEEDBACK)));
     localStorage.removeItem(KEY_FEEDBACK)
     setFeedbackObjFromLocaleStorege()
-    feedbackEmailInput.value ="";
-    feedbackMessageInput.value ="";
+    event.target.value ="";
+    event.target.value ="";
 }
 
 function onInput(event){
-    const currentInput = event.target;
-    switch(currentInput) {
-        case feedbackEmailInput:
-            feedbackObj.email = feedbackEmailInput.value;
+    const currentInputName = event.target.name;
+    switch(currentInputName) {
+        case "email":
+            feedbackObj.email = event.target.value;
           break;
-        case feedbackMessageInput:
-            feedbackObj.message = feedbackMessageInput.value;
+        case "message":
+            feedbackObj.message = event.target.value;
           break;
       }
     localStorage.setItem(KEY_FEEDBACK,JSON.stringify(feedbackObj))
 }
 function setFeedbackObjFromLocaleStorege(){
     try{
-        const localeFeedbackObj = JSON.parse(localStorage.getItem(KEY_FEEDBACK))
-        feedbackObj.email= localeFeedbackObj.email;
-        feedbackObj.message= localeFeedbackObj.message;
+        const {email, message} = JSON.parse(localStorage.getItem(KEY_FEEDBACK))
+        feedbackObj.email= email;
+        feedbackObj.message= message;
     }
     catch{
     }   
