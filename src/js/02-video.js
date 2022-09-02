7c5376724a75a4cb4f485ec1e2c24cb72ec8f1b8
Dafarlog <Dafarlog@gmail.com>
2022-09-02 15:33:04 +0300

    Commit message

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const player = new Player(document.querySelector('iframe'));
+const PLAYER_TIME_KEY = "player-time"
+
+let lastPlayerTime = 0;  
+if(localStorage.getItem(PLAYER_TIME_KEY) != null){
+    lastPlayerTime = localStorage.getItem(PLAYER_TIME_KEY); 
+}
+
+player.on('timeupdate',  throttle(() => {
+    onTimeUpdate()
+  }, 1000));
+
+function onTimeUpdate(){   
+    player.getCurrentTime().then( function(seconds) {
+    localStorage.setItem(PLAYER_TIME_KEY, seconds)
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                console.log(`${error.name}: the time was less than 0 or greater than the video’s duration`)
+            break;
+            default:
+                console.log(error.name)
+             break;
+        }
+    });    
+}
+
+player.setCurrentTime(lastPlayerTime)
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
+// import debounce from 'lodash.debounce';
+const KEY_FEEDBACK ="feedback-form-state";
+const feedbackForm = document.querySelector(".feedback-form")
+const feedbackEmailInput = document.querySelector('[name="email"]')
+const feedbackMessageInput = document.querySelector('[name="message"]')
+const feedbackObj = {
+        email: "",
+        message: "", 
+}
+try{
+    feedbackObj.email= JSON.parse(localStorage.getItem(KEY_FEEDBACK)).email;
+    feedbackObj.message= JSON.parse(localStorage.getItem(KEY_FEEDBACK)).message;
+}
+catch{
+}
 
+feedbackEmailInput.value =feedbackObj.email;
+feedbackMessageInput.value =feedbackObj.message;
+
+feedbackForm.addEventListener("submit", onSubmit)
+
+feedbackForm.addEventListener("input",  throttle((event) => onInput(event), 1000))
+
+function onSubmit(event){
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(KEY_FEEDBACK)));
+    localStorage.clear()
+}
+
+function onInput(event){
+    const currentInput = event.target;
+    switch(currentInput) {
+        case feedbackEmailInput:
+            feedbackObj.email = feedbackEmailInput.value;
+          break;
+        case feedbackMessageInput:
+            feedbackObj.message = feedbackMessageInput.value;
+          break;
+      }
+    localStorage.setItem(KEY_FEEDBACK,JSON.stringify(feedbackObj))
+}
